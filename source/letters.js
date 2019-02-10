d19e58e8311e000e0b579a4fa8a8bf4067f6fcf7
FedorBilichenko <bilichenko.fedor@yandex.ru>
2019-02-10 23:03:12 +0300

    Fixed

--- source/letters.js
+++ source/letters.js
@@ -6,39 +6,33 @@
  * @param {boolean|undefined} mode - flag
  * @returns {string|null} newStr - fixed string
  */
-
-function letters(str, mode) {
+const letters = (str, mode) => {
 
     if (typeof str !== 'string' || (typeof mode !== 'boolean' && mode !== undefined)) {
         return null;
     }
 
-    const alphabet = {};
-    let newStr = '';
-
     if (mode === undefined){
-        for (let symbol of str) {
-            alphabet[symbol] = (alphabet[symbol] || 0) + 1;
-        }
-        for (let symbol of str) {
-            if (alphabet[symbol] === 1) {
-                newStr += symbol;
-            }
-        }
-        return newStr;
-    }
-    if (!mode) {
-        str = [...str].reverse();
-    }
-    for (let symbol of str) {
-        if (!alphabet[symbol]) {
-            newStr += symbol;
-        }
-        alphabet[symbol] = (alphabet[symbol] || 0) + 1;
+        const alphabet = [...str].reduce((alphabet, symbol) => ({
+            ...alphabet,
+            [symbol]: (alphabet[symbol] || 0) + 1
+        }), {});
+        return [...str].filter((symbol) => alphabet[symbol] === 1).join('');
     }
 
-    if (!mode){
-        return [...newStr].reverse().join('');
+    str = [...str];
+    if (!mode) {
+        str = str.reverse();
     }
-    return newStr;
-}
+
+    const alphabet = {};
+    const newStr = str.filter((symbol) => {
+        alphabet[symbol] = (alphabet[symbol] || 0) + 1;
+        return alphabet[symbol] === 1;
+    });
+
+    if (!mode) {
+        return newStr.reverse().join('');
+    }
+    return newStr.join('');
+};
